db31e3a1c337d80d56a77ab28cc678d632d63a69
DmitriyV-V <dimwer1@gmail.com>
2019-07-27 21:21:07 +0700

    hw lesson-5

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,77 @@
+import os
+import sys
+import shutil
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+'''
+def make_dir():
+    for i in range(9):
+        dir_path = os.path.join(os.getcwd(), 'dir_' + str(i + 1))
+        print(dir_path)
+        try:
+            os.mkdir(dir_path)
+        except FileExistsError:
+            print('Такая директория уже существует')
+'''
+
+
+def make_dir(name):
+    try:
+        os.makedirs(name)
+    except FileExistsError:
+        print('Такая директория уже существует')
+
+
+'''
+def del_dir():
+    for i in range(9):
+        dir_path = os.path.join(os.getcwd(), 'dir_' + str(i + 1))
+        print(dir_path)
+        try:
+            os.rmdir(dir_path)
+        except FileExistsError:
+            print('Такой директории не существует')
+'''
+
+
+def del_dir(name):
+    try:
+        os.removedirs(name)
+    except FileExistsError:
+        print('Такой директории не существует')
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+'''
+def list_dir():
+    l_dir = os.listdir(os.getcwd())
+    print(l_dir)
+'''
+
+
+def list_dir():
+    _path = os.listdir()
+    print("Список файлов текущей директории:")
+    for index, element in enumerate(_path, start=1):
+        if os.path.isdir(element):
+            print(f"{index}. {element}")
+
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+
+def copy_file():
+    file_name = os.path.realpath(__file__)
+    new_file = file_name + ".copy"
+
+    if os.path.isfile(new_file) != True:
+        shutil.copy(file_name, new_file)
+        print(f"{new_file} создан")
+    else:
+        print("Файл уже скопирован")
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -1,3 +1,7 @@
+import os
+import hw05_easy as hw5
+
+
 # Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
@@ -13,3 +17,37 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+def dir_change(path):
+    try:
+        os.chdir(path)
+        print(f"Переход в папку {path}")
+    except FileNotFoundError:
+        print(f"Папка dir_{path} не создана")
+
+def menu():
+    answer = ''
+    while answer != 5:
+        answer = input("Выберите действие:\n"
+                       "1. Перейти в папку\n"
+                       "2. Просмотреть содержимое текущей папки\n"
+                       "3. Удалить папку\n"
+                       "4. Создать папку\n"
+                       "5. Выход\n")
+
+        if answer == '1':
+            path_name = input("Укажите папку для перехода: ")
+            print(dir_change(path_name))
+        elif answer == '2':
+            hw5.list_dir()
+        elif answer == '3':
+            path_name = input("Введите имя папки для удаления: ")
+            hw5.del_dir(path_name)
+        elif answer == '4':
+            path_name = input("Введите имя новой папки: ")
+            hw5.make_dir(path_name)
+        elif answer == '5':
+            break
+        else:
+            print("Введено некорректное значение. Пожалуйста, повторите")
+
